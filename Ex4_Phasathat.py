3fdbd020de8ed16ef711e07f428c0ad969b62d26
Phasathat Jaruchitsophon <phasathat.j@ku.th>
2024-02-21 18:44:20 +0700

    Finish

---
+++ Ex4_Phasathat.py
@@ -0,0 +1,12 @@
+Fname = 'Phasathat'
+Lname = 'Jaruchitsophon'
+print("Name:", Fname, Lname)
+print('--- Your Score ---')
+Eng = 98
+Bu = 74
+Comsys = 62
+Compro = 100
+print('Foundation English :', Eng)
+print('General Business :', Bu)
+print('Introduction to Computer Systems :', Comsys)
+print('Computer Programming :', Compro)
